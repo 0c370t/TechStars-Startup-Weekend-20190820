d8402bcdaa4c6c90a533e7f9d5f05de087d107c8
Brian Donald <bdonald@set-works.com>
2019-09-21 16:47:17 -0500

    PUSH!

--- Main/Main.py
+++ Main/Main.py
@@ -9,3 +9,8 @@ def index():
     recent_cleaners = cleaners.getMostRecentCleaners()
     upcoming_tenants = tenants.getTenants()
     return render_template("homeowner.jinja.html", tenants=upcoming_tenants, cleaners=recent_cleaners)
+
+
+@app.route("/2")
+def index2():
+    return render_template("cleaners.master.jinja.html")
