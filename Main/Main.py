36b9b18d44a585aecd28d757c2ce8dc52f665055
Brian Donald <contact@noimbrian.com>
2019-09-21 23:31:19 -0500

    Added Hero Landing Page along with assoc GSheet to collect results

--- Main/Main.py
+++ Main/Main.py
@@ -8,11 +8,4 @@ app.register_blueprint(homeowner)
 
 @app.route("/")
 def index():
-    recent_cleaners = cleaners.getMostRecentCleaners()
-    upcoming_tenants = tenants.getTenants()
-    return render_template("homeowner/home.jinja.html", tenants=upcoming_tenants, cleaners=recent_cleaners)
-
-
-@app.route("/2")
-def index2():
-    return render_template("hospitality/home.jinja.html")
+    return render_template("index.jinja.html")
